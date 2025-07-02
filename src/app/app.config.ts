c2abdaf66990223bc5de6de026248d896ff56875
Tiago Lara <tilara20@gmail.com>
2025-07-01 23:09:55 -0300

    Adicionar o FireBase no projeto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-
 import { routes } from './app.routes';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // Firebase App Initialization
+    provideFirebaseApp(() => initializeApp(environment.firebase))
+  ]
 };
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD01wkrKdVzGwnPXD4kdDSJ8qQnAPQkEHg",
+    authDomain: "listavolei-d644a.firebaseapp.com",
+    projectId: "listavolei-d644a",
+    storageBucket: "listavolei-d644a.appspot.com",
+    messagingSenderId: "763299596159",
+    appId: "1:763299596159:web:df2577b54f517c6a1a7c4a"
+  }
+};
