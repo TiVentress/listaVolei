ddbcf80e3a2c9354f7a254d396821f87d6533ffc
Tiago Lara <tilara20@gmai.com>
2025-10-02 02:00:50 -0300

    :recycle: Refactor: melhorias e ajustes no carrossel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,24 @@
+// Ficheiro: src/app/pages/home/home.component.ts
+
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  // Lista de imagens atualizada com links funcionais
+  images = [
+    'https://images.unsplash.com/photo-1592656094267-764a45160876?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dttps://unsplash.com/pt-br/fotografias/voleibol-amarelo-e-branco-na-areia-marrom-durante-o-dia-VDkRsT649C0https://images.unsplash.com/photo-1592656094267-764a45160876?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://images.unsplash.com/photo-1619472683502-8f245a2a7fce?q=80&w=999&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1548192534-17a5d99133a8?q=80&w=2070&auto-format&fit=crop', 
+    'https://images.unsplash.com/photo-1630435879347-dd1895216501?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?q=80&w=1907&auto-format&fit=crop',
+    'https://images.unsplash.com/photo-1728971121170-2c8bae90d6fb?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1665249820734-2cb83ef75010?q=80&w=875&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  ];
 }
