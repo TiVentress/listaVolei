b0834fc283c7eb828dd17462c0ff00dcf223e4a2
Tiago Lara <tilara20@gmai.com>
2025-10-02 02:30:06 -0300

    :lipstick: Atualizar cor do background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+// Ficheiro: src/app/app.component.ts
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- ADICIONE ESTA IMPORTAÇÃO
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  // ADICIONE CommonModule AQUI
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'listaVolei';
