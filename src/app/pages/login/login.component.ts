d53d28a83f6a49e091d167eac25373026e24b8e9
Tiago Lara <tilara20@gmai.com>
2025-10-03 00:49:37 -0300

    :lipstick: Atualizar estilo do login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,23 +4,28 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
-
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'] // Vamos adicionar o link para o nosso novo CSS
 })
 export class LoginComponent {
-  username = '';
-  password = '';
+  // Agrupamos as credenciais num objeto, como é comum em formulários
+  credentials = {
+    email: '',
+    password: ''
+  };
   error = '';
 
   constructor(private auth: AuthService, private router: Router) { }
 
   async login() {
     try {
-      await this.auth.login(this.username, this.password);
+      // Usamos as propriedades do nosso novo objeto 'credentials'
+      await this.auth.login(this.credentials.email, this.credentials.password);
+      this.error = ''; // Limpa o erro em caso de sucesso
     } catch (err) {
       this.error = 'Usuário ou senha inválidos';
     }
@@ -33,4 +38,3 @@ export class LoginComponent {
     }
   }
 }
-
