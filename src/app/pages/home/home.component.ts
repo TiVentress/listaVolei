e365b82d2bd83da72b330d6f4991af0d8d2a6836
Tiago Lara <tilara20@gmai.com>
2025-10-16 23:18:40 -0300

    :recycle: Refactor: ajustes nos textos da home e remoção de comentários

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,3 @@
-// Ficheiro: src/app/pages/home/home.component.ts
-
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -12,7 +10,6 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  // Lista de imagens atualizada com links funcionais
   images = [
     'https://images.unsplash.com/photo-1592656094267-764a45160876?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dttps://unsplash.com/pt-br/fotografias/voleibol-amarelo-e-branco-na-areia-marrom-durante-o-dia-VDkRsT649C0https://images.unsplash.com/photo-1592656094267-764a45160876?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
     'https://images.unsplash.com/photo-1619472683502-8f245a2a7fce?q=80&w=999&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1548192534-17a5d99133a8?q=80&w=2070&auto-format&fit=crop', 
